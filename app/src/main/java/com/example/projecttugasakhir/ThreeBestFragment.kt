bc75e36216c4b1a985cadd52348aed9b70a43e9c
Andreas Bayu Prakasa <abayup1498@gmail.com>
2024-10-22 11:10:41 +0700

    Update Result and Three Best

---
+++ app/src/main/java/com/example/projecttugasakhir/ThreeBestFragment.kt
@@ -0,0 +1,25 @@
+package com.example.projecttugasakhir
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.projecttugasakhir.databinding.FragmentThreeBestBinding
+
+class ThreeBestFragment : Fragment() {
+    private lateinit var binding:FragmentThreeBestBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_three_best, container, false)
+        binding = FragmentThreeBestBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
